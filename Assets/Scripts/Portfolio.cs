4573f52bc3e3045e3db53243cbabbbd9ca7b5451
Adrian Plapamaru <adrian.plapamaru@yahoo.com>
2025-06-04 18:33:04 +0300

    Resume

--- Assets/Scripts/Portfolio.cs
+++ Assets/Scripts/Portfolio.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Portfolio : MonoBehaviour {
-	
+
+	[SerializeField] private Button resumeButton;
+	[SerializeField] private Button gitButton;
 	[SerializeField] private ScrollRect scrollRect;
 	[SerializeField] private ProjectVideo[] videos;
-
+	
 	private void Awake() {
 		if (!Application.isEditor) {
 			scrollRect.scrollSensitivity = 0.02f;
 		}
+		
+		resumeButton.onClick.AddListener(() => {
+			
+		});
+		gitButton.onClick.AddListener(() => {
+			Application.OpenURL("https://github.com/Plapi");
+		});
+		
 		for (int i = 0; i < videos.Length; i++) {
 			int ii = i;
 			videos[i].Init(() => {
